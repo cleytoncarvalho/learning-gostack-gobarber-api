001304e12880fc4f44a07c97feacb92cc5baad47
Cleyton Carvalho <cleyton.carvalho@outlook.com>
2019-09-02 09:45:46 -0300

    chore: Adjustment for the frontend comsumption

--- src/routes.js
+++ src/routes.js
@@ -22,7 +22,7 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users/:id', UserController.update);
+routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 routes.get('/providers/:providerId/available', AvailableController.index);
